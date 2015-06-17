79be55ea2a05fbedbc190d8aacd2d9451d97ac54
Billy Shen <billy.shen@hetic.net>
2015-06-17 16:32:31 +0200

    [ADD] Sidebar & update header

--- src/app/controllers/dashboardController.js
+++ src/app/controllers/dashboardController.js
@@ -15,7 +15,7 @@ angular
 
       console.log('dashboardCtrl');
 
-    	$http.get('http://travlr.scalingo.io/post').then(function(response) {
+    	$http.get(config.api_url + '/post').then(function(response) {
     		console.log('response', response.data);
         $scope.posts = response.data;
       }, function(err) {
