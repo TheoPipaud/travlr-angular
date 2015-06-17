c8b935e93c3e64a1f5d4cfb82df580a86ee46719
Théo <ac-theo-dc@hotmail.fr>
2015-06-17 16:22:22 +0200

    Activity creation

--- src/app/controllers/activityController.js
+++ src/app/controllers/activityController.js
@@ -9,38 +9,36 @@ angular
     '$scope', '$http', '$state',
     function($scope, $http, $state) {
     	$scope.activity = {};
+        $scope.categories = {};
         if(!$scope.$parent.currentActivity.state){
             $scope.$parent.currentActivity.state = 0;
-            console.log('init');
             $state.go('activity.detail');
         }
-    	$scope.createActivity = function() {
-    		console.log($scope.activity);
-        	$http
-        		.post(config.api_url+'/post', $scope.activity)
-        		.then(function(data){
-        			$state.go('dashboard');
-        		}, function(err){
-        			console.log(err);
-        		})
-        };
-        $scope.checkCustomRadio = function(event){
-            var it = angular.element(event.target);
-            if(!it.hasClass('active')){
-                it.closest('.radio-boxes').find('div').removeClass('active').find('input').prop("checked", false);
-                it.addClass('active');
-                it.find("input").prop("checked", true);
-            }
-            it.closest('.radio-boxes').find(input).prop("checked", false);
-        };
+        $http
+            .get(config.api_url+'/category', $scope.categories)
+            .then(function(data){
+                $scope.categories = data.data;
+            }, function(err){
+                console.log(err);
+            })
+        // $scope.checkCustomRadio = function(event){
+        //     var it = angular.element(event.target);
+        //     if(!it.hasClass('active')){
+        //         it.closest('.radio-boxes').find('div').removeClass('active').find('input').prop("checked", false);
+        //         it.addClass('active');
+        //         it.find("input").prop("checked", true);
+        //     }
+        //     it.closest('.radio-boxes').find(input).prop("checked", false);
+        // };
         $scope.onGuidebarClick = function(url, id){
             console.log($scope.$parent.currentActivity.state);
             if($scope.$parent.currentActivity.state >= (id-1)){
                 $state.go(url);
             }
-        }
+        };
         $scope.submitPart = function(url, id){
             var valid = true;
+
             if(valid){
                 if(parseInt(id)>$scope.$parent.currentActivity.state){
                     $scope.$parent.currentActivity.state = parseInt(id);
@@ -49,7 +47,42 @@ angular
                 $('.guidebar>a:nth-child('+(parseInt(id)+1)+')').addClass('active');
                 $('.guidebar #bar').removeClass().addClass('state-'+(parseInt(id)+1));
                 $('.guidebar .dots>span:nth-child('+(parseInt(id)+1)+')').addClass('active');
-                $state.go(url);
+                
+                if($scope.$parent.currentActivity.values){
+                    $scope.$parent.currentActivity.values = $.extend($scope.$parent.currentActivity.values, $scope.activity);     
+                }else{
+                    $scope.$parent.currentActivity.values = $scope.activity;
+                }
+                if(id == 1){
+                    var selected_categ = [];
+                    $.each($scope.categories, function(id, elem){
+                        if(elem.selected){
+                            selected_categ.push(elem.id);
+                        }
+                    });
+                    $scope.$parent.currentActivity.values.categories = selected_categ;
+                }
+                if(id == 4){
+                    $scope.validateActivity();
+                }else{
+                    $state.go(url);
+                }
+            }
+        };
+        $scope.validateActivity = function(){
+            $http
+                .post(config.api_url+'/post', $scope.$parent.currentActivity.values)
+                .then(function(data){
+                    $scope.$parent.currentActivity.values = {};
+                    $state.go('dashboard');
+                }, function(err){
+                    alert('Veuillez remplir correctement tous les champs !');
+                })
+        };
+        $scope.initInput = function(name){
+            console.log($scope.$parent.currentActivity);
+            if($scope.$parent.currentActivity.values[name]){
+                $scope.activity[name] = $scope.$parent.currentActivity.values[name];
             }
         }
     }  
